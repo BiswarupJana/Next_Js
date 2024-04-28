2df6b540666805001f7fd77028ade190a540a66a
Biswarup Jana <biswaruprx21@gmail.com>
2024-04-28 11:12:46 +0530

    #

--- Newslater/app/news/page.tsx
+++ Newslater/app/news/page.tsx
@@ -1,4 +1,22 @@
 import React from "react";
+import Link from "next/link";
 export default function NewsPage(){
-  return <h1>NewsPage</h1>;
+  return (
+    <>
+    <h1>NewsPage</h1>
+  <ul>
+    <li>
+      <Link href="/news/first-news">First Link</Link>
+    </li>
+    <li>
+      <Link href="/news/second-news">Second Link</Link>
+    </li>
+    <li>
+      <Link href="/news/third-news">Third Link</Link>
+    </li>
+    <li>
+      <Link href="/news/forth-news">Forth Link</Link>
+    </li>
+  </ul></>
+  )
 }
